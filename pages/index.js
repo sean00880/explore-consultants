75804c0373d8f23ae820d1bbd6ed4dbf029613ee
Md Shadhin <shadhin12580@gmail.com>
2023-01-04 18:28:41 +0600

    a

--- pages/index.js
+++ pages/index.js
@@ -20,48 +20,19 @@ export default function Home() {
     return (
         <>
             <Layout HeaderStyle="one">
-                {/* Banner Section One*/}
                 <BannerOne />
-                {/*END Banner Section One */}
-                {/* About Section */}
                 <AboutOne />
-                {/*Emd About Section */}
-                {/* Services Section */}
                 <ServiceOne />
-                {/* End Services Section*/}
-                {/* Offer Section */}
                 <OfferOne />
-                {/* End Offer Section */}
-                {/* Call To Action Two */}
                 <CtaOne />
-                {/*End Call To Action Two */}
-                {/* Project Section */}
                 <ProjectOne />
-                {/*End Project Section */}
-                {/* Testimonial Section */}
-                <TestimonialOne />
-                {/* End Testimonial Section */}
-                {/* Fun Fact Section */}
                 <FunfactOne />
-                {/* End Fun Fact Section */}
-                {/* Team Section */}
                 <TeamOne />
-                {/* End Team Section */}
-                {/* Clients Section   */}
                 <ClientOne />
-                {/*End Clients Section */}
-                {/* FAQ Section */}
                 <FaqOne />
-                {/*End FAQ Section */}
-                {/* News Section */}
                 <BlogOne />
-                {/*End News Section */}
-                {/* Call To Action Two */}
                 <CtaTwo />
-                {/*End Call To Action Two */}
-                {/* Map Section*/}
                 <MapOne />
-                {/*End Map Section*/}
             </Layout>
         </>
     )
