d2a3700d00c75b052267f3fe9f63ac2244277994
Md Shadhin <shadhin12580@gmail.com>
2023-01-14 13:21:11 +0600

    Update ProjectSlider.js

--- components/slider/ProjectSlider.js
+++ components/slider/ProjectSlider.js
@@ -74,7 +74,7 @@ const ProjectSlider = () => {
             >
                 {data.map((item, i) => (
                     <SwiperSlide className="project-block">
-                        <li className="inner-box">
+                        <div className="inner-box">
                             <div className="image-box">
                                 <figure className="image">
                                     <Link href="images/resource/project-1.jpg" className="lightbox-image">
@@ -87,7 +87,7 @@ const ProjectSlider = () => {
                                 <h4 className="title"><Link href="/page-project-details">{item.title}</Link></h4>
                                 <span className="cat">{item.cat}</span>
                             </div>
-                        </li>
+                        </div>
                     </SwiperSlide>
                 ))}
             </Swiper>
