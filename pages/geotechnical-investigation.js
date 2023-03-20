347d25dcc405833b5cbf889b33fef1ff79de700c
sean00880 <theelitereapersanctum@gmail.com>
2023-03-19 20:14:15 -0400

    28th commit

--- pages/geotechnical-investigation.js
+++ pages/geotechnical-investigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from "../components/layout/Layout";
 import PageTitle from "../components/sections/PageTitle1";
-import ServiceDetails from "../components/sections/ServiceDetails";
+import ServiceDetails from "../components/sections/ServiceDetails2";
 
 export default function Home() {
 
